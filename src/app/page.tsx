c233c4a8f91ab087b63f33518b9f3415de9ac3f9
Олег bc2024-5 <olegchaplia2006@gmail.com>
2025-06-27 15:32:06 +0300

    feat: configure and connect the database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,21 @@
+import { prisma } from "@/shared/lib/db";
 import { Button } from "@/shared/ui/button";
+import { Card, CardHeader } from "@/shared/ui/card";
 
-export default function Home() {
+export default async function Home() {
+  const games = await prisma.game.findMany();
+
+  console.log(games);
   return (
-    <Button size="lg" variant="outline">
-      Hello
-    </Button>
+    <div>
+      <Button size="lg" variant="outline">
+        Hello
+      </Button>
+      {games.map((game) => (
+        <Card key={game.id}>
+          <CardHeader>{game.name}</CardHeader>
+        </Card>
+      ))}
+    </div>
   );
 }
